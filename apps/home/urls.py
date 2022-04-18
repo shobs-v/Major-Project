2c775af8154b00ad9d4b42da50a9b4252a86e590
Rucha Nargunde <ruchanargunde@gmail.com>
2022-04-18 13:59:50 +0530

    Front end improved

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -5,13 +5,12 @@ Copyright (c) 2019 - present AppSeed.us
 
 from django.urls import path, re_path
 from apps.home import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
-
     # The home page
     path('', views.new_index, name='home'),
 
-    # Matches any html file
-    re_path(r'^.*\.*', views.pages, name='pages'),
-
 ]
+
+urlpatterns += staticfiles_urlpatterns()
--- apps/home/views.py
+++ apps/home/views.py
@@ -11,44 +11,16 @@ from django.urls import reverse
 from django.shortcuts import render
 
 
-@login_required(login_url="/login/")
-def index(request):
-    context = {'segment': 'index'}
-
-    html_template = loader.get_template('home/index.html')
-    return HttpResponse(html_template.render(context, request))
-
 def new_index(request):
+    output=""
     if request.method == 'POST':
-        text = request.POST['myTextBox']
-        print(text)
-        uploaded_file = request.FILES['document']
-        print(uploaded_file.name)
-        print(uploaded_file.size)
-    return render(request,'home/new_index.html')
+        text = request.POST['inputTextBox']
+        if(text):
+            print(text)
+            output="hi"
+        else:
+            uploaded_file = request.FILES['document']
+            print(uploaded_file.name)
+            print(uploaded_file.size)
 
-
-@login_required(login_url="/login/")
-def pages(request):
-    context = {}
-    # All resource paths end in .html.
-    # Pick out the html file name from the url. And load that template.
-    try:
-
-        load_template = request.path.split('/')[-1]
-
-        if load_template == 'admin':
-            return HttpResponseRedirect(reverse('admin:index'))
-        context['segment'] = load_template
-
-        html_template = loader.get_template('home/' + load_template)
-        return HttpResponse(html_template.render(context, request))
-
-    except template.TemplateDoesNotExist:
-
-        html_template = loader.get_template('home/page-404.html')
-        return HttpResponse(html_template.render(context, request))
-
-    except:
-        html_template = loader.get_template('home/page-500.html')
-        return HttpResponse(html_template.render(context, request))
+    return render(request,'home/new_index.html',{'output':output})
